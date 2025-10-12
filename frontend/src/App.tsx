e54b24a59fa56d502546bb7e3529b6e54ffa1f48
Leone Raulino Silveira <leone.zordenoni@gmail.com>
2025-10-11 22:35:30 -0400

    fix: typing frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,9 @@
-import "./App.css";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { LoginPage } from "./pages/login";
 import { HomePage } from "./pages/home";
 import ManuallyLife from "./pages/manuallyTable";
 
 export const App = () => {
-  // const [count, setCount] = useState(0);
-
   return (
     <BrowserRouter>
       <Routes>

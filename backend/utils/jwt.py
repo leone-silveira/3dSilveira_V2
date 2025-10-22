da48a4618905718494045aa4bbfddbed92385d23
Leone Raulino Silveira <leone.zordenoni@gmail.com>
2025-10-21 20:32:50 -0400

    feat: middleware for router protection

---
+++ backend/utils/jwt.py
@@ -0,0 +1,25 @@
+# utils/jwt_utils.py
+from jose import jwt, JWTError
+from fastapi import HTTPException, status
+from database.config import Settings
+
+
+settings = Settings()
+ALGORITHM = "HS256"
+SECRET_KEY = settings.JWT_SECRET
+
+def verify_jwt_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid token"
+            )
+        return user_id
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token"
+        )

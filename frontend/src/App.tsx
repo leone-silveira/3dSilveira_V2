398ccc8da282eb65ddcdd600ab19e71d005316f4
Leone Raulino Silveira <leone.zordenoni@gmail.com>
2025-10-24 22:19:51 -0400

    feat: side bar initial

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,42 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+} from 'react-router-dom';
 import { LoginPage } from './pages/login';
 import { HomePage } from './pages/home';
 import ManuallyLife from './pages/manuallyTable';
-import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import TemporaryDrawer from './components/sidebar';
 
 const queryClient = new QueryClient();
+function Filho() {
+  const location = useLocation();
+  const hideDrawer = location.pathname === '/login';
+
+  
+  return (
+    <>
+      {!hideDrawer && <TemporaryDrawer />}{' '}
+      {/* Só mostra Drawer fora do login */}
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/ManuallyLife" element={<ManuallyLife />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </>
+  );
+}
 
 export const App = () => {
   return (
-    <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/ManuallyLife" element={<ManuallyLife />} />
-            <Route path="*" element={<Navigate to="/login" />} />
-          </Routes>
-        </BrowserRouter>
-      </QueryClientProvider>
-    </React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Filho />
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 };

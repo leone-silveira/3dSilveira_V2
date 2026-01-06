9e614b95571f79074c409b3cfedd0880901a5bcc
Leone Raulino Silveira <leone.zordenoni@gmail.com>
2026-01-06 11:42:28 -0400

    fix: simplify logic of router list

--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -7,23 +7,26 @@ import {
 import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { SideBarBox } from './styles';
-import { sideBarList } from '../../configs/barList';
 import { SystemContext } from '../../context/useSystem';
 import KeyboardDoubleArrowRightTwoToneIcon from '@mui/icons-material/KeyboardDoubleArrowRightTwoTone';
 import KeyboardDoubleArrowLeftTwoToneIcon from '@mui/icons-material/KeyboardDoubleArrowLeftTwoTone';
+import { routerList } from '../../configs/routerList';
+import { resolvePath } from '../../utils/utils';
 
 export const Sidebar: React.FC = () => {
   const [open, setOpen] = useState(false);
   const navigate = useNavigate();
   const { system } = useContext(SystemContext);
   const currentPage =
-    sideBarList.find((item) => item.name === system)?.optionList || [];
+    routerList.find((item) => item.name === system)?.children || [];
+  const currentPageFiltered = currentPage.filter((item) => item.path !== '');
+
   return (
     <SideBarBox isOpened={open}>
-      {currentPage.map((item) => (
-        <ListItemButton key={item.name} onClick={() => navigate(item.path)}>
+      {currentPageFiltered.map((item) => (
+        <ListItemButton key={item.name} onClick={() => navigate(resolvePath(system, item.path))}>
           <ListItemIcon sx={{ justifyContent: 'center' }}>
-            <item.icon />
+            {item.icon && <item.icon />}
           </ListItemIcon>
           {open && <ListItemText primary={item.name} />}
         </ListItemButton>
